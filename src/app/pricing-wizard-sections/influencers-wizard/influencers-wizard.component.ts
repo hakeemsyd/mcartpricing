d1ff40e26f6e5d8c64ae6e1e8288099782eaca9f
Waleed Farooqi <wmfarooqi05@gmail.com>
2019-03-09 04:26:43 +0500

    Added influencers data from local json file

--- src/app/pricing-wizard-sections/influencers-wizard/influencers-wizard.component.ts
+++ src/app/pricing-wizard-sections/influencers-wizard/influencers-wizard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import InfluencersJSON from '../../mock_data/influencers.json';
 
 @Component({
   selector: 'app-influencers-wizard',
   templateUrl: './influencers-wizard.component.html',
   styleUrls: ['./influencers-wizard.component.css',
-  '../../pricing-wizard/pricing-wizard.component.css']
+    '../../pricing-wizard/pricing-wizard.component.css']
 })
 export class InfluencersWizardComponent implements OnInit {
 
@@ -21,69 +22,70 @@ export class InfluencersWizardComponent implements OnInit {
   followersArr = [500, 1000, 5000, 10000];
   currFollowersVal = this.followersArr[0];
 
-  influencersList = [
-    {
-      name: 'Blake Benson',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSum9a9eBrxWLQ6JOPTCFIvlN4WFyi4K1wwTEeeTODwPsUCLR_SvA',
-      id: 1,
-      isChecked: false,
-    },
-    {
-      name: 'Merve Kaya',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4XtuSS9gqQHhE3AvWZd7uT1nH0I8cTrkKoYGRaWpAFn6HRmIO1Q',
-      id: 2,
-      isChecked: false,
-    },
-    {
-      name: 'David Michael',
-      img: 'https://www.dogrulukpayi.com/image/actor/binali-yildirim/orig.png',
-      id: 3,
-      isChecked: true,
-    },
-    {
-      name: 'Ahmet Bey',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGA_Rm90mfQazbQM9_5BuhYrftDiOf1Fv-RyQCRwM-zVUYGz4g3A',
-      id: 4,
-      isChecked: false,
-    },
-    {
-      name: 'Augusta Fitzgerald',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoOilLqp99d98H-su5a4UVL_tZX5zy7iY5gxla2QwQcptH8XuCHw',
-      id: 5,
-      isChecked: false,
-    },
-    {
-      name: 'Savas Mart',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtis6fD8BtlhzgrWUYLeury-EmDsRpppkVtkou5RyRSBrfJIJH',
-      id: 6,
-      isChecked: false,
-    },
-    {
-      name: 'Chester Carson',
-      img: 'https://foto.sondakika.com/haber/2017/10/26/izmir-de-feto-sanigi-avukat-kilic-in-dosyasi-10175393_o.jpg',
-      id: 7,
-      isChecked: false,
-    },
-    {
-      name: 'Brent Elliot',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM6uciOQmx2kowfFaYviOr8Ploj7GJGH1EIZsHdpWk_AUEImWn',
-      id: 8,
-      isChecked: false,
-    },
-    {
-      name: 'Jacqueline Persons',
-      img: 'https://www.icmp.int/wp-content/uploads/2012/02/icmp-commissioner-hm-queen-noor-of-jordan.jpg',
-      id: 9,
-      isChecked: false,
-    },
-    {
-      name: 'Gordon Persons',
-      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUukX06jIx-u-bqqeQp712tm10YWo6d9azj5vvyXEQgzKqmYR',
-      id: 10,
-      isChecked: false,
-    },
+  influencersList = InfluencersJSON;
+  // [
+  //   {
+  //     name: 'Blake Benson',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSum9a9eBrxWLQ6JOPTCFIvlN4WFyi4K1wwTEeeTODwPsUCLR_SvA',
+  //     id: 1,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Merve Kaya',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4XtuSS9gqQHhE3AvWZd7uT1nH0I8cTrkKoYGRaWpAFn6HRmIO1Q',
+  //     id: 2,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'David Michael',
+  //     img: 'https://www.dogrulukpayi.com/image/actor/binali-yildirim/orig.png',
+  //     id: 3,
+  //     isChecked: true,
+  //   },
+  //   {
+  //     name: 'Ahmet Bey',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGA_Rm90mfQazbQM9_5BuhYrftDiOf1Fv-RyQCRwM-zVUYGz4g3A',
+  //     id: 4,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Augusta Fitzgerald',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoOilLqp99d98H-su5a4UVL_tZX5zy7iY5gxla2QwQcptH8XuCHw',
+  //     id: 5,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Savas Mart',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtis6fD8BtlhzgrWUYLeury-EmDsRpppkVtkou5RyRSBrfJIJH',
+  //     id: 6,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Chester Carson',
+  //     img: 'https://foto.sondakika.com/haber/2017/10/26/izmir-de-feto-sanigi-avukat-kilic-in-dosyasi-10175393_o.jpg',
+  //     id: 7,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Brent Elliot',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM6uciOQmx2kowfFaYviOr8Ploj7GJGH1EIZsHdpWk_AUEImWn',
+  //     id: 8,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Jacqueline Persons',
+  //     img: 'https://www.icmp.int/wp-content/uploads/2012/02/icmp-commissioner-hm-queen-noor-of-jordan.jpg',
+  //     id: 9,
+  //     isChecked: false,
+  //   },
+  //   {
+  //     name: 'Gordon Persons',
+  //     img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUukX06jIx-u-bqqeQp712tm10YWo6d9azj5vvyXEQgzKqmYR',
+  //     id: 10,
+  //     isChecked: false,
+  //   },
 
-  ];
+  // ];
 
   constructor(private fb: FormBuilder) {
 
