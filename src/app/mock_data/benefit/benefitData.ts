fb04b9b84384b00b2811f8fc69e8724082c5acc4
Waleed Farooqi <wmfarooqi05@gmail.com>
2019-03-15 20:08:53 +0500

    Benefit Table with carousel added

--- src/app/mock_data/benefit/benefitData.ts
+++ src/app/mock_data/benefit/benefitData.ts
@@ -7,7 +7,7 @@ export const MediaBenefitItem: IBenefitItem[] = [
     description: 'Tap into the vast potential of omnichannel commerce. mCart lets you establish your own omnichannel marketplaces and partnerships with top retailers to monetize the digital, video and print content you’re already creating by making it shop-able. See what kind of revenue you should expect based on current site traffic.',
     mainImage: [
       'marketplace-revenue-site-traffic-table.png',
-      'contextual-commerce.png',
+      'contextual-commerce-impression-purchase.png',
     ],
   },
   {
@@ -16,7 +16,7 @@ export const MediaBenefitItem: IBenefitItem[] = [
     description: 'In mCart marketplaces, content sell products and products sell content! Every mCart is linked to original content . mCart marketplaces converge media and commerce, centralizing your entire content base from social and/or mainstream in one location.',
     mainImage: [
       'miss-arizona-maddie-rose-holler-magzine-cover.png',
-      'jewelery-cart-menu.png',
+      'jewelery-cart-menu.jpg',
     ],
   },
   {
@@ -24,7 +24,8 @@ export const MediaBenefitItem: IBenefitItem[] = [
     icon: 'advertiser-retention.png',
     description: 'Use mCart to track every banner, billboard, commercial, infomercial, and advertisement campaign to sales dollars. Keep your advertisers on board by proving the ROI of their TV, print, and digital media advertising.',
     mainImage: [
-      'dashboard-influencers-productivity-report-chart.png'
+      'dashboard-influencers-productivity-report-chart.png',
+      'dashboard-influencers-productivity-report-chart-zoom.png'
     ],
   },
   {
@@ -32,8 +33,8 @@ export const MediaBenefitItem: IBenefitItem[] = [
     icon: 'managing-external-salesforce.png',
     description: 'Your original content is influencing articles, blogs, and social posts, and their creators can commercially exploit it without giving you a penny. mCart allows those creators to be part of your team and get paid without any back-office accounting through a digital wallet on blockchain.',
     mainImage: [
-      'contextual-commerce.png',
-      'featured-mcarts-products-tables',
+      'contextual-commerce-impression-purchase.png',
+      'featured-mcarts-products-tables.png',
     ],
   },
   {
@@ -42,25 +43,113 @@ export const MediaBenefitItem: IBenefitItem[] = [
     description: 'Control everything about your mCart marketplace using a powerful yet intuitive editorial dashboard. The platform is highly modular, meaning it can be customized to your needs with minimal engineering.',
     mainImage: [
       'total-sales-snapshot-report-chart.png',
-      'mCart-items-list.png'],
+      'mCart-items-list.png',
+    ],
   },
   {
     heading: 'Actionable Data & CRM',
-    icon: 'data-and-crm.png',
+    icon: 'actionabledata-and-crm.png',
     description: 'Get data about every step along the path to purchase to learn more about your audience-turned-customers and what’s driving them to buy or pass. Then put all that data to use with mCart’s AI-driven CRM.',
     mainImage: [
-      'iMac-display-monitor-with-complete-info.png',]
+      'iMac-display-monitor-with-complete-info.png',
+    ],
   },
   {
     heading: 'Social media centralization and monetization',
     icon: 'social-media-centralization.png',
     description: 'mCarts are integrated with all major social media platforms, letting you monetize your brand’s social influence on consumers.',
     mainImage: [
-      'mCart-social-media-share-links-url-qrcode.png',],
+      'mCart-social-media-share-links-url-qrcode.png'],
   },
 ];
-export const CPGBenefit: IBenefitItem[] = [];
+export const CPGBenefit: IBenefitItem[] = [
+  {
+    heading: 'Direct to Consumers ',
+    icon: 'direct-to-Consumers.png',
+    description: 'Create an mCart marketplace to bring your products directly to the consumer. Form direct consumer relationships, get better product-market fit data, and make more informed decisions about inventory management, sales and marketing.',
+    mainImage: [
+      'featured-mCart-consumer-products.png',
+    ],
+  },
+  {
+    heading: 'Multi-Store Checkout ',
+    icon: 'multi-store-checkout.png',
+    description: 'mCarts engages shoppers in the content they’re already consuming, giving them a 360-degree view of the products with lookalikes, real-time coupons, featured content, and buying options. At the end, mCart places shoppers in the stores’ checkouts.',
+    mainImage: [
+      'patent-pending-items-store-checkout.png'],
+  },
+  {
+    heading: 'Seamless Channel Integration & Centralization',
+    icon: 'seamless-channel-integration-centralization.png',
+    description: 'The mCart integration layer seamlessly connects to your channel’s e-commerce engines, omnichannel inventory management, POS systems through mCart’s powerful inventory ingestion and attribution tracking APIs and tag manager.',
+    mainImage: [
+      'mCart-channel-integration-through-powerful-inventory-ingestion-and-attribution-tracking-apis.png'],
+  },
+  {
+    heading: 'Smart & Centralized Trade Promotions',
+    icon: 'Smart-Centralized-trade-promotions.png',
+    description: 'mCart’s AI-powered, dynamic and personalized promotion management portal let CPGs control their trade-spend efficiency by sending adjustable and segmented promotions only to consumers who are already active on the path to purchase.',
+    mainImage: [
+      'trade-promotion-ad-product-medinatura.png',
+      'add-new-personal-promo-code.png',
+      'trade-promotion-price-drop-alerts.png',
+    ],
+  },
+  {
+    heading: 'Shop in Real-World Context',
+    icon: 'shop-in-real-world-context.png',
+    description: 'mCart Shop-able Walls on Sharp interactive displays and mCarts posted behind content make the mCart cross-touchpoint shopping experience possible. Shoppers discover products in real-world context, and then send carts to their phone, share with others or communicate with on-site salespeople.',
+    mainImage: [
+      'shopping-real-world-context.png',
+      'contexual-commerce-impression-purchase.png',
+    ],
+  },
+  {
+    heading: 'A Full Control Turnkey Solution',
+    icon: 'control-trunkey-solution.png',
+    description: 'Control everything about your mCart marketplace using powerful yet intuitive controls. The platform is highly modular, meaning it can be customized to your needs with minimal engineering.',
+    mainImage: [
+      'total-sales-snapshot-report-chart.png',
+      'mCart-items-list.png',
+    ],
+  },
+  {
+    heading: 'Actionable Data & CRM',
+    icon: 'actionable-data-crm.png',
+    description: 'Get data about every step along the path to purchase to learn more about your customers and what’s driving them to buy. Then put all that data to use with mCart’s AI-driven CRM.',
+    mainImage: [
+      'mcart-ai-driven-crm-after-knowing-your-customer-buying-motivation.png',
+    ],
+  },
+  {
+    heading: 'Social Media Centralization & Monetization',
+    icon: 'social-media-centralization-monetization.png',
+    description: 'mCarts are integrated with all major social media platforms, letting you monetize your brand’s social influence on consumers.',
+    mainImage: [
+      'mCart-social-media-share-links-url-qrcode.png',
+      'contexual-commerce-impression-purchase.png',
+    ],
+  },
+
+];
+export const ProcurementBenefit: IBenefitItem[] = [
+  {
+    heading: 'Easy Onboarding & Processing',
+    icon: 'easy-onboarding-processing.png',
+    description: 'Onboarding is easy. Aggregate all your vendors and their inventories on your marketplace with a streamlined system. Then drive down processing costs for your purchases with a centralized, easy-to-use marketplace that monitors controls and compliance while generating savings and revenue for your organization.',
+    mainImage: [
+      'featured-mCart-multiple-consumer-products.png',
+    ],
+  },
+  {
+    heading: 'Affordable Affiliate Marketing',
+    icon: 'affordable-affiliate-marketing.png',
+    description: 'Your vendors often pay Amazon over 40% of their sales. They will be eager to work directly with your marketplace when you charge them a more reasonable affiliate fee and extend your marketplace to your employees’ families and friends to gain commission on their purchases.',
+    mainImage: [
+      'marketplace-commision-in-purchases-for-family-friends-employees.png',
+    ],
+  },
+];
 export const MallsBenefit: IBenefitItem[] = [];
-export const ProcurementBenefit: IBenefitItem[] = [];
 export const AgencyBenefit: IBenefitItem[] = [];
 export const OtherBenefit: IBenefitItem[] = [];
--- src/app/plan-info-wizard/benefit-calculator/benefit-calculator.component.ts
+++ src/app/plan-info-wizard/benefit-calculator/benefit-calculator.component.ts
@@ -2,41 +2,45 @@ import { Component, OnInit } from '@angular/core';
 import { benefits as BENEFITS, IBenefit, benefits } from 'src/app/mock_data/benefit/benefit';
 import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 import { HttpClient } from '@angular/common/http';
-
+import { image } from '../../../assets/images/'
 @Component({
   selector: 'app-benefit-calculator',
   templateUrl: './benefit-calculator.component.html',
   styleUrls: ['./benefit-calculator.component.css'],
   providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
+
 export class BenefitCalculatorComponent implements OnInit {
 
   showNavigationArrows = false;
   showNavigationIndicators = false;
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
-
-
-  benefits: IBenefit = BENEFITS[0];
+  benefits: IBenefit[] = BENEFITS;
+  selectedBenefit: IBenefit;
   constructor(config: NgbCarouselConfig) {
     console.log('benefits', this.benefits);
     // customize default values of carousels used by this component tree
-      config.showNavigationArrows = true;
-      config.showNavigationIndicators = true;
+    config.showNavigationArrows = true;
+    config.showNavigationIndicators = true;
+    this.selectedBenefit = this.benefits[0];
   }
 
   ngOnInit() {
   }
 
   getImageName(name) {
-    console.log('name', name);
-    if (name === 1) {
-      return 'https://www.sign.nl/wp-content/uploads/2016/10/mcart-Large.jpg';
-    }
-    return 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDIIqgzItFlNkxALQlnyhSjgK8bmM4Dpj8U1vTn9aFg12Gj6uggw';
+    return `../../../assets/images/${name}`;
   }
 
   getIconName(name) {
-    return 'https://www.sibon.nl/wp-content/uploads/2016/06/Logo-McArt.jpg';
+    return `../../../assets/icons/${name}`;
+  }
+
+  isImageArray(image) {
+    if (image && image.length > 1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
