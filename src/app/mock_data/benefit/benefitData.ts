b0368c1e239b4613a9fdc61ac8c684b899ca716a
Arslan Sarwar <marslansarwar171@gmail.com>
2019-03-22 12:44:48 +0500

    Added missing benefits to benefits list Fixed Plan table spelling and css issues

--- src/app/mock_data/benefit/benefitData.ts
+++ src/app/mock_data/benefit/benefitData.ts
@@ -227,5 +227,116 @@ export const MallsBenefit: IBenefitItem[] =  [
     ],
   },
 ];
-export const AgencyBenefit: IBenefitItem[] = [];
-export const OtherBenefit: IBenefitItem[] =  [];
+export const AgencyBenefit: IBenefitItem[] = [
+  {
+    heading: 'Content Monetization',
+    icon: 'content-monetization.png',
+    description: 'Tap into the vast potential of omnichannel commerce. mCart lets you establish your own omnichannel marketplaces and partnerships with top retailers to monetize the digital, video and print content you’re already creating by making it shop-able. See what kind of revenue you should expect based on current site traffic.',
+    mainImage: [
+      'marketplace-revenue-site-traffic-table.png',
+      'contextual-commerce-impression-purchase.png',
+    ],
+  },
+  {
+    heading: 'User Acquisition',
+    icon: 'user-acquisition.png',
+    description: 'In mCart marketplaces, content sell products and products sell content! Every mCart is linked to original content . mCart marketplaces converge media and commerce, centralizing your entire content base from social and/or mainstream in one location.',
+    mainImage: [
+      'miss-arizona-maddie-rose-holler-magzine-cover.png',
+      'jewelery-cart-menu.jpg',
+    ],
+  },
+  {
+    heading: 'Advertiser Retention',
+    icon: 'advertiser-retention.png',
+    description: 'Use mCart to track every banner, billboard, commercial, infomercial, and advertisement campaign to sales dollars. Keep your advertisers on board by proving the ROI of their TV, print, and digital media advertising.',
+    mainImage: [
+      'dashboard-influencers-productivity-report-chart.png',
+      'dashboard-influencers-productivity-report-chart-zoom.png'
+    ],
+  },
+  {
+    heading: 'Managing An External Salesforce with No Back Office Efforts',
+    icon: 'managing-external-salesforce.png',
+    description: 'Your original content is influencing articles, blogs, and social posts, and their creators can commercially exploit it without giving you a penny. mCart allows those creators to be part of your team and get paid without any back-office accounting through a digital wallet on blockchain.',
+    mainImage: [
+      'contextual-commerce.jpg',
+      'featured-mcarts-products-tables.png',
+    ],
+  },
+  {
+    heading: 'A Full- Control Turnkey Solution ',
+    icon: 'control-trunkey-solution.png',
+    description: 'Control everything about your mCart marketplace using a powerful yet intuitive editorial dashboard. The platform is highly modular, meaning it can be customized to your needs with minimal engineering.',
+    mainImage: [
+      'total-sales-snapshot-report-chart.png',
+      'mCart-items-list.png',
+    ],
+  },
+  {
+    heading: 'Actionable Data & CRM',
+    icon: 'actionable-data-crm.png',
+    description: 'Get data about every step along the path to purchase to learn more about your audience-turned-customers and what’s driving them to buy or pass. Then put all that data to use with mCart’s AI-driven CRM.',
+    mainImage: [
+      'iMac-display-monitor-with-complete-info.png',
+    ],
+  },
+  {
+    heading: 'Social media centralization and monetization',
+    icon: 'social-media-centralization.png',
+    description: 'mCarts are integrated with all major social media platforms, letting you monetize your brand’s social influence on consumers.',
+    mainImage: [
+      'mCart-social-media-share-links-url-qrcode.png'],
+  },
+
+];
+export const OtherBenefit: IBenefitItem[] =  [
+  {
+    heading: 'New Omnichannel Revenue',
+    icon: 'new-omnichannel-revenue.png',
+    description: 'Don’t stay siloed in the world of brick & mortar and lose business to e-commerce. Use mCart to create local and universal omnichannel marketplaces to sell in person and online, closing the gap between your offline and online worlds.',
+    mainImage: [
+      'close-gap-between-offline-and-online-world-with-omnichannel-revenue.png',
+    ],
+  },
+  {
+    heading: 'Build Direct Relationships with Shoppers',
+    icon: 'build-direct-relationships-with-shoppers.png',
+    description: 'Engage shoppers directly and build relationships with them. With your mCart branded marketplace, create shop-able content, utilize a salesforce of influencers, and offer personalized offers.',
+    mainImage: [],
+  },
+  {
+      heading: 'Influencer Marketing',
+      icon: 'influencer-marketing.png',
+      description: 'All influencers—from your aunt to the biggest Hollywoods stars—can make mCart smart shopping carts, promote them on social media and in content, and seamlessly receive payment for their influence using blockchain.',
+      mainImage: [
+        'create-and-promote-mcart-in-3-easy-steps.png',
+        'contexual-commerce-impression-purchase.png',
+      ],
+  },
+  {
+    heading: 'Shop in Real-World Context',
+    icon: 'shop-in-real-world-context.png',
+    description: 'mCart Shop-able Walls on Sharp interactive displays and mCarts posted behind content make cross-touchpoint shopping experience possible. Shoppers discover products in real-world context, and then send their carts from content or Walls to their phone, share with others or communicate with on-site salespeople.',
+    mainImage: [
+      'shopping-real-world-context.png',
+    ],
+  },
+  {
+    heading: 'A Full Control Turnkey Solution',
+    icon: 'a-full-Control-turnkey-solution.png',
+    description: 'Control everything about your mCart marketplace using powerful yet intuitive controls. The platform is highly modular, meaning it can be customized to your needs with minimal engineering.',
+    mainImage: [
+      'total-sales-snapshot-report-chart.png',
+      'mCart-items-list.png',
+    ],
+  },
+  {
+    heading: 'Actionable Data & CRM',
+    icon: 'actionable-data-crm.png',
+    description: 'Get data about every step along the path to purchase to learn more about your customers and what’s driving them to buy. Then put all that data to use with mCart’s AI-driven CRM.',
+    mainImage: [
+      'mcart-ai-driven-crm-after-knowing-your-customer-buying-motivation.png',
+    ],
+  },
+];
